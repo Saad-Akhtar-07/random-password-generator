3365c02c89e2ce06c65bf20aeaa6ee44d17096b8
Saad-Akhtar-07 <saadakhtar.cp07@gmail.com>
2025-09-21 15:33:04 +0500

    Tailwind setup complete and states are initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback } from 'react'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [AllowNumber, setAllowNumber] = useState(false)
+  const [AllowCharacter, setAllowCharacter] = useState(false)
+  const [password, setPassword] = useState("")
+
+
+  const passwordGenerator = useCallback(() => {
+    const password = ""
+    const string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(AllowNumber) string += "0123456789"
+    if(AllowCharacter) string += "!@#$%^&*()_+"
+
+    for(let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * string.length + 1)
+      password += string.charAt(char)
+    }
+    setPassword(password)
+    
+  }, [length, AllowNumber, AllowCharacter])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+ 
+
+    <div className="flex items-center justify-center w-full max-w-md mx-auto shadow-lg rounded-lg px-6 py-4 my-8 text-orange-500 bg-gray-800 ">
+      <div className="flex items-center justify-between shadow-md rounded-lg overflow-hidden mb-7">
+      <h1 className="flex items-center justify-center text-xl font-bold  text-center text-white">
+      Password Generator
+    </h1>
+        <input
+          type="text"
+          value={password}
+          className="w-full p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-white"
+          placeholder="Password"
+          readOnly
+        />
+        <button className="p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-white ml-2">copy</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  </>
   )
 }
 
